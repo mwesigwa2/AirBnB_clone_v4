447e89c7f116d0e5da6a631be4d19a1d92f324ba
Timothy Kibirige <tmwesigwa2@gmail.com>
2023-09-20 15:03:48 +0000

    MySQL setup test

--- setup_mysql_test.sql
+++ setup_mysql_test.sql
@@ -1,7 +1,9 @@
---- create A database hbnb_test_db
---- A new user hbnb_test (in localhost)
-CREATE DATABASE IF NOT EXISTS 'hbnb_test_db';
-CREATE USER IF NOT EXISTS 'hbnb_test'@'localhost';
-SET PASSWORD FOR 'hbnb_test'@'localhost' = 'hbnb_test_pwd';
+-- Script that prepares a MySQL server for the project
+-- with a database hbnb_test_db
+-- and a new user hbnb_test (in localhost)
+CREATE DATABASE IF NOT EXISTS hbnb_test_db;
+CREATE USER IF NOT EXISTS 'hbnb_test'@'localhost' IDENTIFIED BY 'hbnb_test_pwd';
 GRANT ALL PRIVILEGES ON hbnb_test_db.* TO 'hbnb_test'@'localhost';
-GRANT SELECT ON performance_schema.* TO 'hbnbhbnb_test'@'localhost';
+FLUSH PRIVILEGES;
+GRANT SELECT ON performance_schema.* TO 'hbnb_test'@'localhost';
+FLUSH PRIVILEGES;
