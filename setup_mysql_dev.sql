a81a4b2869a6a83d9036b7cc0b8d9e4c5296e189
kazibweyassin <kazibweusama@gmail.com>
2023-09-20 04:55:47 -0700

    setting up sql development

---
+++ setup_mysql_dev.sql
@@ -0,0 +1,6 @@
+--- create A database hbnb_dev_db
+--- A new user hbnb_dev (in localhost)
+CREATE DATABASE IF NOT EXISTS 'hbnb_dev_db'@'localhost';
+SET PASSWORD FOR 'hbnb_dev'@'localhost' = 'hbnb_dev_pwd';
+GRANT ALL PREVILEGES ON hbnb_dev_db.* TO 'hbnb_dev'@'localhost';
+GRANT SELECT ON performance_schema .* TO 'hbnb_dev'@'localhost';
