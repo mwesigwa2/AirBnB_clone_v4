7d0d0dc4965bcddc68522d4541e1b5f4de8036b1
kazibweyassin <kazibweusama@gmail.com>
2023-09-20 09:15:06 -0700

    Delete obj from __objects if it exists

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -48,3 +48,9 @@ class FileStorage:
                         self.all()[key] = classes[val['__class__']](**val)
         except FileNotFoundError:
             pass
+
+    def delete(self, obj=None):
+        """Delete obj from __objects if it exists"""
+        if obj:
+            keys = "{}.{}".format(type(obj).__name__, obj.id)
+            self.__objects.pop(keys, None)
