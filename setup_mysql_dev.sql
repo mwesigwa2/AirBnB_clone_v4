031a3bfbc74add7d5118049737476cde7d968251
Timothy Kibirige <tmwesigwa2@gmail.com>
2023-09-20 14:36:05 +0000

    script that prepares MySQL server

--- setup_mysql_dev.sql
+++ setup_mysql_dev.sql
@@ -1,6 +1,7 @@
---- create A database hbnb_dev_db
---- A new user hbnb_dev (in localhost)
-CREATE DATABASE IF NOT EXISTS 'hbnb_dev_db'@'localhost';
-SET PASSWORD FOR 'hbnb_dev'@'localhost' = 'hbnb_dev_pwd';
+--- script that prepares a MySQL server for the project
+CREATE DATABASE IF NOT EXISTS hbnb_dev_db;
+CREATE USER IF NOT EXISTS 'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
 GRANT ALL PRIVILEGES ON hbnb_dev_db.* TO 'hbnb_dev'@'localhost';
+FLUSH PRIVILEGES;
 GRANT SELECT ON performance_schema.* TO 'hbnb_dev'@'localhost';
+FLUSH PRIVILEGES;
