13725fde672850a587af40194f888e6ddba6455c
Timothy Kibirige <tmwesigwa2@gmail.com>
2023-10-22 22:45:55 +0000

    update files

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -37,7 +37,7 @@ class DBStorage:
             Base.metadata.drop_all(self.__engine)
 
     def all(self, cls=None):
-        """Query on the curret database session all objects of the given class.
+        """Query on the current database session all objects of the given class.
         If cls is None, queries all types of objects.
         Return:
             Dict of queried classes in the format <class name>.<obj id> = obj.
