705ba834e4e04ec4eba5111c06c74b65b0d13113
Timothy Kibirige <tmwesigwa2@gmail.com>
2023-11-01 13:50:13 +0000

    update users.py

--- api/v1/views/users.py
+++ api/v1/views/users.py
@@ -4,8 +4,6 @@ from api.v1.views import app_views
 from flask import jsonify, abort, request, make_response
 from models import storage
 from models.user import User
-from datetime import datetime
-import uuid
 
 
 @app_views.route('/users', methods=['GET'], strict_slashes=False)
@@ -20,19 +18,19 @@ def list_users():
 
 @app_views.route('/users/<user_id>', methods=['GET'], strict_slashes=False)
 def get_user(user_id):
-    '''Retrieves a User object'''
-    all_users = storage.get(User, user_id)
-    if all_users:
-        return jsonify(all_users.to_dict())
+    """Retrieves a User object"""
+    user = storage.get(User, user_id)
+    if user:
+        return jsonify(user.to_dict())
     abort(404)
 
 
 @app_views.route('/users/<user_id>', methods=['DELETE'], strict_slashes=False)
 def delete_user(user_id):
     '''Deletes a User object'''
-    all_users = storage.get(User, user_id)
-    if all_users:
-        storage.delete(all_users)
+    user = storage.get(User, user_id)
+    if user:
+        storage.delete(user)
         storage.save()
         return make_response(jsonify({}), 200)
     abort(404)
@@ -64,6 +62,6 @@ def updates_user(user_id):
         abort(400, 'Not a JSON')
     for key, value in data.items():
         if key not in ['id', 'created_at', 'updated_at']:
-            setattr(user. key, vallue)
+            setattr(user. key, value)
     user.save()
     return jsonify(user.to_dict()), 200
